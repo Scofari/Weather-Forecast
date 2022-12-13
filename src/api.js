e5caee9c1abf1017e7dfeb6a68d4af5bb047d96c
Vladimir <scofari99@gmail.com>
2022-12-13 10:53:06 +0200

    full project

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "74aaeb3d3cmsh05d78610795804cp1fd241jsncb02a20ad94a",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "1ce6c2057f0f2f17359075570bcd09a6";
